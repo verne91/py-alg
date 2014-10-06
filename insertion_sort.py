6b0b702592883a3cff2aee0c769984db5a11f848
csun <verne91@gmail.com>
2014-10-06 16:55:20 +0800

    Insertion Sort version 1

---
+++ insertion_sort.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+#Insertion Sort ---- Non-descending order
+def insertion_sort(A):
+	for j in xrange(1,len(A)):
+		key = A[j]
+		i = j - 1							#insert A[j] into sorted sequence A[1...j-1]
+		while i > 0 and A[i] > key:
+			A[i+1] = A[i]
+			i -= 1
+		A[i+1] = key
+	return A
+	
+if __name__ == '__main__':
+	a = [1,3,5,3,89,5,44,2]
+	insertion_sort(a)
+	print a
