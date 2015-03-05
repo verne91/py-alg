52ee2714dd87b6d70e2772cbc17fce0f7db48c56
Chen Sun <verne91@gmail.com>
2015-03-05 11:04:46 +0800

    fix a small bug and add command line input

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
+import sys
 
 #Insertion Sort ---- Non-descending order
 def insertion_sort(A):
@@ -11,13 +12,15 @@ def insertion_sort(A):
 	for j in xrange(1,len(A)):
 		key = A[j]
 		i = j - 1							#insert A[j] into sorted sequence A[1...j-1]
-		while i > 0 and A[i] > key:
+		while i >= 0 and A[i] > key:
 			A[i+1] = A[i]
 			i -= 1
 		A[i+1] = key
 	return A
 	
 if __name__ == '__main__':
-	a = [1,3,5,3,89,5,44,2]
-	insertion_sort(a)
-	print a
+	numlist = map(int, sys.argv[1:])
+	print insertion_sort(numlist)
+#	a = [1,3,5,3,89,5,44,2]
+#	insertion_sort(a)
+#	print a
